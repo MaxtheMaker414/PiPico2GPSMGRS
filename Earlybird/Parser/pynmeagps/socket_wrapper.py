942fe7c7a92465bb29c78c8ac744595c16d71a3d
MaxtheMaker414 <86385388+MaxtheMaker414@users.noreply.github.com>
2025-03-23 17:54:36 +0100

    Delete Earlybird directory

--- Earlybird/Parser/pynmeagps/socket_wrapper.py
+++
@@ -1,8 +0,0 @@
-"""
-Dummy-Klasse für SocketWrapper, um MicroPython-Kompatibilität zu gewährleisten.
-"""
-
-class SocketWrapper:
-    """Leere Klasse, da MicroPython kein `socket`-Modul benötigt."""
-    def __init__(self, *args, **kwargs):
-        pass
